19c4d877340efaa3652153ef0eeb89c80c968b79
carlinw <carlin.williamson1@gmail.com>
2019-05-19 23:30:01 +1000

    argparse. can now specify a team and pdf filename

--- drawToIcal.py
+++ drawToIcal.py
@@ -3,10 +3,13 @@ import re
 import PyPDF2
 import pytz
 import datetime
+import argparse
 
 GROUNDS = ["Pennant Hills 1", "Pennant Hills 2", "Sutherland", "Ryde", "Cintra Park", "Lidcombe", "Bankstown", "Olympic 1", "Olympic 2", "Daceyville"]
 
+# To make the regex easier we convert team names to a Capital letter followed by lower case letters with no spaces
 TEAM_REPLACEMENTS = {"UNSW":"Unsw", "UTS":"Uts", "Northern Districts":"Northerndistricts", "NWS/Baulkham Hills":"NWSBaulkhamhills", "Ryde HH":"Ryde"}
+INV_TEAM_REPLACEMENTS = {j: k for k,j in TEAM_REPLACEMENTS.items()}
 
 MONTH_TO_NUMBER = {"Jan":1, "Feb":2, "Mar":3, "Apr":4, "May":5, "Jun":6, "Jul":7, "Aug":8, "Sep":9, "Oct":10, "Nov":11, "Dec":12}
 
@@ -26,19 +29,27 @@ class Game:
 		minutes = int(time.split(":")[1])
 		d = datetime.datetime(year, month, day, hours, minutes)
 		timezone = pytz.timezone("Australia/Sydney")
+
 		self.date = timezone.localize(d)
 		self.ground = ground
 		self.time = time
-		self.home = home
-		self.away = away
+
+		# change the team names back
+		self.home = INV_TEAM_REPLACEMENTS.get(home, home)
+		self.away = INV_TEAM_REPLACEMENTS.get(away, away)
 	
 	def __str__(self):
 		return "{} {} {}\n{} vs {}".format(self.time, self.date, self.ground, self.home, self.away)
 
-pdf = ""
+parser = argparse.ArgumentParser(description='Convert a pdf of a Sydney Hockey draw into an ical')
+parser.add_argument("filename", help='the file from which the ical should be created')
+parser.add_argument('--team', dest='team', help='specify a team for which the events are parse')
+args = parser.parse_args()
+
 
 # read the pdf and create one long string
-with open("doc.pdf", 'rb') as f:
+pdf = ""
+with open(args.filename, 'rb') as f:
 	reader = PyPDF2.PdfFileReader(f)
 	for page in reader.pages:
 		pdf += page.extractText()
@@ -51,8 +62,7 @@ pdf = re.sub(r'Round.*?\d\d\d\d', r'', pdf)
 # changing them to be the same 
 pdf = re.sub(r'Pitch ', r'', pdf)
 
-# change team names so they have a leading capital followed by lower case letters
-# this makes the regex is easier as there is no spaces or markers after we import the pdf
+# Apply the dictionary to the whole string
 for key, value in TEAM_REPLACEMENTS.items():
         pdf = pdf.replace(key, value)
 
@@ -75,21 +85,31 @@ for i, index in enumerate(ground_indexes):
 	else:
 		g = pdf[index:ground_indexes[i+1]]
 
-	for (ground, date, time, home, away, otherGames) in re.findall(r'(.*?)DayTimeHomeAway(.*?)(\d\d:\d\d)([A-Z\/]+[a-z_]*)([A-Z\/]+[a-z_]*)(.*)', g):
+	for (ground, date, time, home, away, other_games) in re.findall(r'(.*?)DayTimeHomeAway(.*?)(\d\d:\d\d)([A-Z\/]+[a-z_]*)([A-Z\/]+[a-z_]*)(.*)', g):
 		matches.append(Game(ground, date, time, home, away))
 
 		# There can be more than one game at a specific ground in that round
 		# keep parsing those games until we run out of them
-		while (len(otherGames) > 5):
-			reg = re.search(r'(.*?)(\d\d:\d\d)([A-Z\/]+[a-z_]*)([A-Z\/]+[a-z_]*)(.*)', other)
+		while len(other_games) > 5:
+			reg = re.search(r'(.*?)(\d\d:\d\d)([A-Z\/]+[a-z_]*)([A-Z\/]+[a-z_]*)(.*)', other_games)
 			matches.append(Game(ground, reg.group(1), reg.group(2), reg.group(3), reg.group(4)))
-			otherGames = reg.group(5)
+			other_games = reg.group(5)
 
 # convert our list of matches into a iCalendar
 c = Calendar()
 for match in matches:
-	e = Event(name="{} vs {}".format(match.home, match.away), begin=match.date, location=match.ground)
+	name = ""
+	if not args.team:
+		name = "{} vs {}".format(match.home, match.away)
+	elif args.team in match.home:
+		name = "{} vs {}".format(args.team, match.away)
+	elif args.team in match.away:
+		name = "{} vs {}".format(args.team, match.home)
+	else:
+		continue
+
+	e = Event(name=name, begin=match.date, location=match.ground)
 	c.events.add(e)
 
-with open('matches.ics', 'w') as my_file:
+with open(args.filename.split(".")[0] + ".ics", "w") as my_file:
 	my_file.writelines(c)
